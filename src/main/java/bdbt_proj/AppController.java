392a98509fd38a44d07afefca025e769b6b8d83e
Jakub Wojciechowski <01159566@pw.edu.pl>
2022-01-16 14:12:47 +0100

    basic Spring Security added, html names changed

--- src/main/java/bdbt_proj/AppController.java
+++ src/main/java/bdbt_proj/AppController.java
@@ -1,6 +1,7 @@
 package bdbt_proj;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -8,43 +9,61 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
+@Configuration
 @Controller
-public class AppController {
+public class AppController implements WebMvcConfigurer {
     @Autowired
     private OperatorzyDAO operatorzyDAO;
     @Autowired
     private AdresyDAO adresyDAO;
 
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/main").setViewName("main");
+        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/operatorData").setViewName("operatorData"); TODO is it needed?
+    }
+
     @RequestMapping("/")
-    public String viewHomePage(Model model){
-        List<Operator> operatorList = operatorzyDAO.list();
-        model.addAttribute("operatorList", operatorList);
+    public String viewHomePage(Model model) {
         return "index";
     }
+
+
+    @RequestMapping("/operatorData")
+    public String viewOperatorDataPage(Model model) {
+        List<Operator> operatorList = operatorzyDAO.list();
+        model.addAttribute("operatorList", operatorList);
+        return "operatorData";
+    }
+
     @RequestMapping("/new")
-    public String viewClickPage(Model model){
+    public String viewClickPage(Model model) {
         Operator operator = new Operator();
         Adres adres = new Adres();
-        model.addAttribute("operator",operator);
-        model.addAttribute("adres",adres);
+        model.addAttribute("operator", operator);
+        model.addAttribute("adres", adres);
         return "new_Operator";
     }
-    @RequestMapping(value = "/save", method= RequestMethod.POST)
-    public String save(@ModelAttribute("operator") Operator operator, @ModelAttribute("adres") Adres adres){
 
-        if(adresyDAO.validate(adres)==Boolean.TRUE) {
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("operator") Operator operator, @ModelAttribute("adres") Adres adres) {
+        if (adresyDAO.validate(adres) == Boolean.TRUE) {
             String adresId = adresyDAO.save(adres);
             operator.setNr_adresu(adresId);
             operatorzyDAO.save(operator);
-            return "redirect:/";
-        }
-        else return "redirect:/error";
+            return "redirect:/operatorData";
+        } else return "redirect:/input_error";
     }
+
     @RequestMapping("/edit/{Nr_operatora}")
-    public ModelAndView showEditForm(@PathVariable(name="Nr_operatora") String Nr_operatora){
+    public ModelAndView showEditForm(@PathVariable(name = "Nr_operatora") String Nr_operatora) {
         ModelAndView mav = new ModelAndView("edit_Operator_form");
         Operator operator = operatorzyDAO.get(Nr_operatora);
         Adres adres = adresyDAO.get(operator.getNr_adresu()); //stad adres dla danego operatora B)
@@ -54,18 +73,20 @@ public class AppController {
         System.out.println(adres);
         return mav; //czemu nie po prostu String edit_Operator_form?
     }
+
     @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public String update(@ModelAttribute("operator") Operator operator, @ModelAttribute("adres") Adres adres){
+    public String update(@ModelAttribute("operator") Operator operator, @ModelAttribute("adres") Adres adres) {
         System.out.println(operator);
         System.out.println(adres);
         operatorzyDAO.update(operator);
         adres.setNr_adresu(operator.getNr_adresu());
         adresyDAO.update(adres);
-        return "redirect:/";
+        return "redirect:/operatorData";
     }
+
     @RequestMapping("/delete/{nr_operatora}")
-    public String delete(@PathVariable(name="nr_operatora") String nr_operatora){
+    public String deleteOperator(@PathVariable(name = "nr_operatora") String nr_operatora) {
         operatorzyDAO.delete(nr_operatora);
-        return "redirect:/";
+        return "redirect:/operatorData";
     }
 }
---
+++ src/main/java/bdbt_proj/SecurityConfiguration.java
@@ -0,0 +1,47 @@
+package bdbt_proj;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("user")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/main")
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
